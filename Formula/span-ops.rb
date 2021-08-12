692c1afbd84e54a9ab2318049bf39b51020010a1
goreleaserbot <goreleaser@spandigital.com>
2021-08-12 13:49:03 +0200

    Brew formula update for span-ops version v0.1.8

--- Formula/span-ops.rb
+++ Formula/span-ops.rb
@@ -5,28 +5,28 @@
 class SpanOps < Formula
   desc "span-ops allows you to interact with the supplier services that SPAN uses."
   homepage "https://github.com/SPANDigital/span-ops"
-  version "0.1.7"
+  version "0.1.8"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/SPANDigital/span-ops/releases/download/v0.1.7/span-ops_0.1.7_Darwin_x86_64.tar.gz", :using => GitDownloadStrategy
-      sha256 "eb7f39ea01a3a1d11e2ba8b6eba64bba97075524b767f743752e706e58c23401"
+      url "https://github.com/SPANDigital/span-ops/releases/download/v0.1.8/span-ops_0.1.8_Darwin_x86_64.tar.gz"
+      sha256 "0cfb9e427ed3564987e5cb55808492fff4afd00ddb5256159643b17699ad99b7"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/SPANDigital/span-ops/releases/download/v0.1.7/span-ops_0.1.7_Darwin_arm64.tar.gz", :using => GitDownloadStrategy
-      sha256 "2df648816634bd191d96b0650b055225ac074b1572e941387a595f2cbc7d5a17"
+      url "https://github.com/SPANDigital/span-ops/releases/download/v0.1.8/span-ops_0.1.8_Darwin_arm64.tar.gz"
+      sha256 "91485ce1f5624cb1792082ba31fcfdda6ca2fe3f23c928016ee5585135b1a9d7"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/SPANDigital/span-ops/releases/download/v0.1.7/span-ops_0.1.7_Linux_x86_64.tar.gz", :using => GitDownloadStrategy
-      sha256 "e50ef178890fb7e42ecdc53d41c6da5061f57179ed5f840126b9ba95d78dca24"
+      url "https://github.com/SPANDigital/span-ops/releases/download/v0.1.8/span-ops_0.1.8_Linux_x86_64.tar.gz"
+      sha256 "205aaa7f19e605c66b01d78676754dd7f142b8437b7653d44c5b0cdab04e5aaa"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/SPANDigital/span-ops/releases/download/v0.1.7/span-ops_0.1.7_Linux_arm64.tar.gz", :using => GitDownloadStrategy
-      sha256 "1f51175440914132e9c8d66a3cf336e46abccc555269b9fb11e344417321e49f"
+      url "https://github.com/SPANDigital/span-ops/releases/download/v0.1.8/span-ops_0.1.8_Linux_arm64.tar.gz"
+      sha256 "8a4ecfe98fc0c892d22db26d86db357dd3292553dbdc26df447a1013671a5203"
     end
   end
 
